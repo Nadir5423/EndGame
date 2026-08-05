d4df3103d3ff48fc3b2845cbcd9cba5dbb0083f3
Nadir Ahmad <nadirahmad5423@gmail.com>
2026-08-05 11:28:14 +0500

    main

---
+++ src/Header/header.jsx
@@ -0,0 +1,11 @@
+export default function Header()
+{
+    return(
+       <header className="header-continer">
+
+         <h1>Assembly</h1>
+         <p>Guess the word within 8 attempts to keep the
+                programming world safe from Assembly</p>
+       </header>
+    )
+}
